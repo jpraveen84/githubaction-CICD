43edbbfa1ae56ef8303c3012cb6acd2273cf0b78
jpraveen84 <42113117+jpraveen84@users.noreply.github.com>
2023-03-16 08:57:38 +0000

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World-12');
+  res.send('Hello World-123');
 });
 
 /* istanbul ignore next */
